18c06a08c9b1fd1cc5604a516393df173e530f44
thanhtung3496 <33385100+thanhtung3496@users.noreply.github.com>
2019-11-22 11:49:25 -0500

    factor DOMStrings

--- app.js
+++ app.js
@@ -5,22 +5,54 @@ var budgetController = (function(){
     
 })();
 
+
+
+
 /*UI CONTROLLER*/
 var UIController = (function(){
-    
-    
+    var DOMStrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    }
+    return {
+        getInput: function (){
+            return {
+            // addType will have either of two values: "inc" for + or "exp" for -
+            type: document.querySelector(DOMStrings.inputType).value,
+            description: document.querySelector(DOMStrings.inputDescription).value,
+            value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+        
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
     
 })();
 
+
+
+
+
 /*GLOBAL APP CONTROLLER*/
 var controller = (function(budgetCtrl, UICtrl){
+    
+    var DOMStrings = UICtrl.getDOMStrings();
+    
     var ctrlAddItem = function(){
-        
+        //1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+            
+        //2. Add the item to the budget controller
         
         
     };
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMStrings.addBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event){
         // check if Enter is pressed
